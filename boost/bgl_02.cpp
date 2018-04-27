7dec219d3b5162959c77bbc402679ca92b8de968
jaideep-khadilkar <jaideep.khadilkar@gmail.com>
2018-04-26 21:14:59 -0700

    bgl

--- boost/bgl_02.cpp
+++ boost/bgl_02.cpp
@@ -5,6 +5,8 @@
 #include <boost/graph/properties.hpp>
 #include <boost/property_map/property_map.hpp>
 #include <boost/graph/named_function_params.hpp>
+#include <boost/graph/stoer_wagner_min_cut.hpp>
+#include <boost/graph/one_bit_color_map.hpp>
 
 struct VertexData
 {
@@ -13,6 +15,26 @@ struct VertexData
 
 void example0a()
 {
+
+	int imageData[9];
+	imageData[0] = 100;
+	imageData[1] = 90;
+	imageData[2] = 10;
+	imageData[3] = 90;
+	imageData[4] = 80;
+	imageData[5] = 05;
+	imageData[6] = 80;
+	imageData[7] = 10;
+	imageData[8] = 0;
+
+	float tLinkFG[9];
+	float tLinkBG[9];
+	for(int i=0;i<9;i++)
+	{
+		tLinkFG[i] = 1 - float(abs(imageData[i]-100))/float(100);
+		tLinkBG[i] = 1 - float(abs(imageData[i]-0))/float(100);
+	}
+
   typedef boost::adjacency_list<boost::vecS, boost::vecS,
                                 boost::undirectedS,
 								VertexData,
@@ -22,12 +44,25 @@ void example0a()
   boost::property_map<MyGraphType, boost::edge_weight_t>::type weightmap =
     get(boost::edge_weight, G);
 
-  for(int i=0;i<10;i++)
+  for(int i=0;i<9;i++)
   {
-	  auto e = add_edge(i,i+1,G).first;
-	  weightmap[e] = i;
-	  std::cout << "saved weight " << weightmap[e] << std::endl;
+	  auto e1 = add_edge(9,i,G).first;
+	  weightmap[e1] = tLinkFG[i];
+	  std::cout << "FG : " << weightmap[e1] << std::endl;
+
+	  auto e2 = add_edge(10,i,G).first;
+	  weightmap[e2] = tLinkBG[i];
+	  std::cout << "BG : " << weightmap[e2] << std::endl;
+
   }
+
+  auto parity = boost::make_one_bit_color_map(num_vertices(G), get(boost::vertex_index, G));
+  float mincut = boost::stoer_wagner_min_cut(G, weightmap, boost::parity_map(parity));
+  auto vpair = vertices(G);
+  for(auto iter=vpair.first; iter!=vpair.second; iter++) {
+    std::cout << "Parity " << get(parity,*iter) << std::endl;
+  }
+
 
 }
 
