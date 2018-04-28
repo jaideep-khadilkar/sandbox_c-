d0019e2560f1513295b17a258a6f3cc27112e61f
jaideep-khadilkar <jaideep.khadilkar@gmail.com>
2018-04-28 06:09:10 -0700

    image segmentation

--- boost/bgl_03.cpp
+++ boost/bgl_03.cpp
@@ -8,7 +8,9 @@
 #include <boost/graph/stoer_wagner_min_cut.hpp>
 #include <boost/graph/one_bit_color_map.hpp>
 #include "ppm.h"
+#include <boost/timer.hpp>
 
+using namespace std;
 struct VertexData
 {
 	int value;
@@ -16,6 +18,10 @@ struct VertexData
 
 void example0a()
 {
+
+	/*
+	 * Read Image
+	 */
 	const int width = 64;
 	const int height = 64;
 	const int size = width * height;
@@ -23,6 +29,9 @@ void example0a()
 
 	ppm	image("/home/user/git/sandbox_cpp/boost/apples_64.ppm");
 
+	/*
+	 * Create Graph
+	 */
   typedef boost::adjacency_list<boost::vecS, boost::vecS,
                                 boost::undirectedS,
 								VertexData,
@@ -32,8 +41,22 @@ void example0a()
   boost::property_map<MyGraphType, boost::edge_weight_t>::type weightmap =
     get(boost::edge_weight, G);
 
-  int pdfFG[numLevels];
-  int pdfBG[numLevels];
+  /*
+   * Set Neighboring Weights
+   */
+
+
+
+
+
+
+
+  /*
+   * Build Histogram & Probability Distribution Function
+   */
+
+  int pdfFG[numLevels] = {0};
+  int pdfBG[numLevels] = {0};
   int sampleCountFG = 0;
   int sampleCountBG = 0;
   for(int i=0;i<size;i++)
@@ -50,33 +73,51 @@ void example0a()
 	  }
   }
 
+  cout << "sampleCountFG : " << sampleCountFG << endl;
+  cout << "sampleCountBG : " << sampleCountBG << endl;
+
+  cout << "FG Histogram " << endl;
   for(int i=0;i<numLevels;i++)
   {
-	  if(sampleCountFG!=0)
-	  {
-		  pdfFG[i] = pdfFG[i]/sampleCountFG;
-	  }
-	  if(sampleCountBG!=0)
-	  {
-		  pdfBG[i] = pdfBG[i]/sampleCountBG;
-	  }
+	  if(pdfFG[i]!=0)
+	  cout << i << " : " << pdfFG[i] << endl;
+
+  }
+  cout << "BG Histogram " << endl;
+  for(int i=0;i<numLevels;i++)
+  {
+	  if(pdfBG[i]!=0)
+	  cout << i << " : " << pdfBG[i] << endl;
   }
 
+  /*
+   * Set Regional Weights
+   */
   for(int i=0;i<size;i++)
   {
 	  int data = int(image.g[i]);
+
 	  auto e1 = add_edge(size,i,G).first;
-	  weightmap[e1] = 1 - float(abs(data-255))/float(255);
+//	  weightmap[e1] = 1 - float(abs(data-255))/float(255);
+	  weightmap[e1] = float(pdfFG[data])/float(sampleCountFG);
+//	  cout << "FG : " << weightmap[e1] << endl;
 
 	  auto e2 = add_edge(size+1,i,G).first;
-	  weightmap[e2] = 1 - float(abs(data-0))/float(255);
-
+//	  weightmap[e2] = 1 - float(abs(data-0))/float(255);
+	  weightmap[e2] = float(pdfBG[data])/float(sampleCountBG);
+//	  cout << "BG : " << weightmap[e2] << endl;
   }
 
+  /*
+   * MinCut Algorithm
+   */
   auto parity = boost::make_one_bit_color_map(num_vertices(G), get(boost::vertex_index, G));
   float mincut = boost::stoer_wagner_min_cut(G, weightmap, boost::parity_map(parity));
   auto vpair = vertices(G);
 
+  /*
+   * Write Out The Segmented Image
+   */
   int currentVtxIndex = 0;
   for(auto iter=vpair.first; iter!=vpair.second; iter++) {
     image.r[currentVtxIndex] = get(parity,*iter) * 255;
@@ -92,5 +133,7 @@ void example0a()
 int
 main(int, char *[])
 {
+  boost::timer t; // start timing
   example0a();
+  cout << t.elapsed() << endl;
 }
